3e86dece2ab2edebdb24f49aa0e1023e6fc9ad08
miyake <myk19977@gmail.com>
2025-01-01 19:28:10 +0900

    アイコン修正

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
+      includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.svg"],
       manifest: {
         name: "サブスクリプション管理",
         short_name: "サブスク管理",
@@ -16,19 +16,19 @@ export default defineConfig({
         background_color: "#F7FAFC",
         icons: [
           {
-            src: "pwa-192x192.png",
+            src: "pwa-192x192.svg",
             sizes: "192x192",
-            type: "image/png",
+            type: "image/svg+xml",
           },
           {
-            src: "pwa-512x512.png",
+            src: "pwa-512x512.svg",
             sizes: "512x512",
-            type: "image/png",
+            type: "image/svg+xml",
           },
           {
-            src: "pwa-512x512.png",
+            src: "pwa-512x512.svg",
             sizes: "512x512",
-            type: "image/png",
+            type: "image/svg+xml",
             purpose: "maskable",
           },
         ],
